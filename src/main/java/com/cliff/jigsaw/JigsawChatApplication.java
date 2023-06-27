ce95b2096e070c411aaa7637d7b8ea130cf9b002
baekenough <baekenough@gmailc.om>
2023-06-27 18:10:01 +0900

    first commit

---
+++ src/main/java/com/cliff/jigsaw/JigsawChatApplication.java
@@ -0,0 +1,13 @@
+package com.cliff.jigsaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigsawChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JigsawChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cliff/jigsaw/JigsawChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cliff.jigsaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigsawChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
